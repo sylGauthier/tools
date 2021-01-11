fb29c18b6b86a40a629e242607fddc661fc0b2b5
Sylvain Gauthier <syg@pedantic.software>
2021-01-11 12:33:26 +1100

    fix dwm font

--- savedconfig/x11-wm/dwm-6.2.h
+++ savedconfig/x11-wm/dwm-6.2.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Hack:pixelsize=12:antialias=true" };
 static const char dmenufont[]       = "Hack:pixelsize=12:antialias=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -33,7 +33,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
